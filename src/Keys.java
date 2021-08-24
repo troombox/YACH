3be400e9372a85e0cbf5e008d6022bd9a10efc36
SemionRudman <45360723+troombox@users.noreply.github.com>
2021-08-24 20:15:08 +0300

    added: keyPress(), keyRelease(), getLastKeyPressed()

--- src/Keys.java
+++ src/Keys.java
@@ -4,11 +4,13 @@ public class Keys {
     public static final int KEYS_NUM = 0xf;
     private boolean[] _keys;
     private boolean _keyFlag;
+    private int _lastKey;
 
     public Keys(){
         _keys = new boolean[KEYS_NUM];
         IntStream.range(0x0,KEYS_NUM).forEach(i -> _keys[i] = false);
         _keyFlag = false;
+        _lastKey = -1;
     }
 
     public boolean getKeyState(int key){
@@ -18,18 +20,39 @@ public class Keys {
         return _keys[key];
     }
 
-    public void setKeyState(int key, boolean state){
-        if(key < 0 || key >= KEYS_NUM){
+    public void setKeyState(int key, boolean state) {
+        if (key < 0 || key >= KEYS_NUM) {
             throw new IllegalArgumentException("KEY not exists");
         }
         _keys[key] = state;
     }
 
-    public void setKeyFlag(boolean state){
+    public void keyPress(int key) {
+        if (key < 0 || key >= KEYS_NUM) {
+            throw new IllegalArgumentException("KEY not exists");
+        }
+        _keys[key] = true;
+        _keyFlag = true;
+        _lastKey = key;
+    }
+
+    public void keyRelease(int key) {
+        if (key < 0 || key >= KEYS_NUM) {
+            throw new IllegalArgumentException("KEY not exists");
+        }
+        _keys[key] = false;
+        _lastKey = -1;
+    }
+
+    public int getLastKeyPressed() {
+        return _lastKey;
+    }
+
+    public void setKeyFlag(boolean state) {
         _keyFlag = state;
     }
 
-    public boolean getKeyFlag(){
+    public boolean getKeyFlag() {
         return _keyFlag;
     }
 }
